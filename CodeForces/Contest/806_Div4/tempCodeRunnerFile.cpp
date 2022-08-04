d48c788cd61cb3645bd5d4508a98037b07bfa02a
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2022-08-05 00:33:48 +0600

    Code

---
+++ CodeForces/Contest/806_Div4/tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+    // for(int i=0; i<n; i++) {
+
+    //     for(int j=0; j<mvs[i]; j++) {
+    //         if(moves[i].at(j)=='U') {
+    //             if(mvs[i]==0)
+    //                 mvs[i]=9;
+    //             else
+    //                 mvs[i]--;
+    //         }
+            
+    //         else if(moves[i].at(j)=='D') {
+    //             if(mvs[i]==9)
+    //                 mvs[i]=0;
+    //             else
+    //                 mvs[i]++;
+    //         }
+
+    //     }
+        
+    // }
